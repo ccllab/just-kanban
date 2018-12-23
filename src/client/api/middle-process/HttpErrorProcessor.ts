5724de0e087632f4d05dca1cb4523cce5837503d
JayZang <sa841218as@gmail.com>
2018-12-23 13:51:51 +0800

    移除尚未登入時，網頁初始之錯誤顯示

--- src/client/api/middle-process/HttpErrorProcessor.ts
+++ src/client/api/middle-process/HttpErrorProcessor.ts
@@ -1,18 +1,22 @@
 import { failMiddleProcess } from '../../utils/ApiRequestor'
 import store from '../../store'
-import { types } from '../../store/types'
+import { types as rootTypes } from '../../store/types'
+import { types as authTypes } from '../../store/auth/types'
 
 export const HttpErrorProcessor: failMiddleProcess = (res) => {
   switch (res.response.status) {
     case 401:
-      store.dispatch(types.SHOW_ERROR, {
+      let isAuthenticated = store.getters[authTypes.IS_AUTHENTICATED]
+      if (!isAuthenticated) break
+
+      store.dispatch(rootTypes.SHOW_ERROR, {
         msg: '權限不足！',
         duration: 3000
       })
       break
 
     default:
-      store.dispatch(types.SHOW_ERROR, {
+      store.dispatch(rootTypes.SHOW_ERROR, {
         msg: res.response.data.error,
         duration: 3000
       })
